f3cafc3889bf17c18453a488ff669fc5c1bb7304
Moana <sabiu7108@gmail.com>
2024-04-26 00:18:44 +0900

    Add support for new data sources

--- routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const authController = require("../controllers/authController");
-
-// Auth routes
-router.post("/register", authController.registerAdmin);
-router.post("/login", authController.login);
-
-module.exports = router;
